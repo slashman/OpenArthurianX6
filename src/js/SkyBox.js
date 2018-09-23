dc529339dc8fe6ee310daeabb759fbacd33147f4
slash <slash@slashware.net>
2018-09-22 21:31:14 -0500

    skybox - Update color of skybox background based on time

--- src/js/SkyBox.js
+++ src/js/SkyBox.js
@@ -1,15 +1,22 @@
 const PlayerStateMachine = require('./PlayerStateMachine');
 const Timer = require('./Timer');
+const SkyColor = require('./SkyColor');
 
-// TODO: Update color of skybox background based on time
+const skyboxRadius = 50;
+const skyboxPosition = {
+  x: 350,
+  y: 50
+};
+const skyboxMargin = 15;
 
 const SkyBox = {
   init(game, layer) {
     this.game = game;
     this.skyboxLayer = this.game.add.group(layer);
-    this.skySprite = this.game.add.sprite(0, 0, 'skies', 0, this.skyboxLayer);
-    this.skySprite.anchor.x = 0.5;
-    this.skySprite.anchor.y = 1;
+    this.skyBack = this.game.add.graphics(skyboxPosition.x, skyboxPosition.y, this.skyboxLayer);
+    this.skyBack.anchor.x = 0.5;
+    this.skyBack.anchor.y = 1;
+
     this.sunSprite = this.game.add.sprite(0, 0, 'celestialBodies', 0, this.skyboxLayer);
     this.sunSprite.anchor.x = 0.5;
     this.sunSprite.anchor.y = 0.5;
@@ -20,50 +27,44 @@ const SkyBox = {
     this.moonSprite.visible = false;
     this.currentMinuteOfDay = 8 * 60;
 
-    this.mask = this.game.add.graphics(0, 0, this.skyboxLayer);
+    this.mask = this.game.add.graphics(this.skyBack.x, this.skyBack.y, this.skyboxLayer);
     this.mask.beginFill(0xffffff);
-    this.mask.drawRect(-this.skySprite.width/2, -this.skySprite.height, this.skySprite.width, this.skySprite.height);
+    this.mask.drawRect(-skyboxRadius, -skyboxRadius, 2 * skyboxRadius, skyboxRadius);
   
     this.sunSprite.mask = this.mask;
-    this.moonSprite.mask = this.mask;
-
-    this.updateTimeOfDay();
+    this.moonSprite.mask = this.mask
+    this.skyBack.mask = this.mask
   },
   timeOfDayPass: function(){
     if (PlayerStateMachine.state !== PlayerStateMachine.WORLD){
-      Timer.delay(1000).then(() => this.timeOfDayPass());
+      Timer.delay(100).then(() => this.timeOfDayPass());
       return;
     }
     this.updateTimeOfDay();
   },
   updateTimeOfDay() {
     this.currentMinuteOfDay += 5;
-    if (this.currentMinuteOfDay > 60 * 24) {
+    if (this.currentMinuteOfDay >= 60 * 24) {
       this.currentMinuteOfDay = 0;
     }
     const currentHourOfDay = this.currentMinuteOfDay / 60; 
-    const skyboxRadius = 30;
-    const skyboxPosition = {
-      x: 350,
-      y: 50
-    };
-    this.skySprite.x = skyboxPosition.x;
-    this.skySprite.y = skyboxPosition.y;
-    // TODO: Add mask as a child of skySprite
-    this.mask.x = this.skySprite.x;
-    this.mask.y = this.skySprite.y;
-
+    const currentMinuteOfHour = ((this.currentMinuteOfDay % 60) / 60) * 100;
+    if (currentMinuteOfHour === 100) {
+      currentMinuteOfHour = 0;
+    }
+    const currentHHMM = Math.floor(currentHourOfDay) * 100 + currentMinuteOfHour;
+    this.skyBack.beginFill(SkyColor.getColor(currentHHMM));
+    this.skyBack.drawCircle(0, 0, 2 * skyboxRadius);
     const hourIncrement = (2 * Math.PI) / 24;
     const sunRads = (currentHourOfDay + 6) * hourIncrement;
     const moonRads = (currentHourOfDay + 6 + 12) * hourIncrement;
-    // console.log(`Current hour is ${currentHourOfDay}, radians are ${sunRads}`);
-    const xPos = Math.cos(sunRads) * skyboxRadius;
-    const yPos = Math.sin(sunRads) * skyboxRadius;
-    this.sunSprite.x = xPos + skyboxPosition.x;
-    this.sunSprite.y = yPos + skyboxPosition.y;
+    const xPos = Math.cos(sunRads) * (skyboxRadius - skyboxMargin);
+    const yPos = Math.sin(sunRads) * (skyboxRadius - skyboxMargin);
+    this.sunSprite.x = Math.round(xPos + skyboxPosition.x);
+    this.sunSprite.y = Math.round(yPos + skyboxPosition.y);
     this.sunSprite.visible = true;
-    this.moonSprite.x = Math.cos(moonRads) * skyboxRadius + skyboxPosition.x;
-    this.moonSprite.y = Math.sin(moonRads) * skyboxRadius + skyboxPosition.y;
+    this.moonSprite.x = Math.round(Math.cos(moonRads) * (skyboxRadius - skyboxMargin) + skyboxPosition.x);
+    this.moonSprite.y = Math.round(Math.sin(moonRads) * (skyboxRadius - skyboxMargin) + skyboxPosition.y);
     this.moonSprite.visible = true;
     Timer.delay(1000).then(()=>this.timeOfDayPass());
   },
