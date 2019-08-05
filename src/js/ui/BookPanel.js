a711a30ee5d214a74942b426ba767fbf5b6186e7
Slash <slash@slashware.net>
2019-08-04 21:45:40 -0500

    Allow passing book page with the mouse

--- src/js/ui/BookPanel.js
+++ src/js/ui/BookPanel.js
@@ -3,17 +3,32 @@ const Phaser = require('phaser');
 const FONT_SIZE = 12;
 const PAGE_LINES = 11;
 const PAGE_WIDTH = 89;
+const RIGHT_PAGE_LOCATION = 110;
+const PAGE_MARGIN_X = 10;
+const PAGE_MARGIN_Y = 8;
+const BOOK_LOCATION_X = 90;
+const BOOK_LOCATION_Y = 77;
 
 const BookPanel = {
     init(game, layer) {
         this.game = game;
         this.layer = this.game.add.group(layer);
-        this.layer.x = 90;
-        this.layer.y = 77;
+        this.layer.x = BOOK_LOCATION_X;
+        this.layer.y = BOOK_LOCATION_Y;
         this.background = game.add.image(0, 0, "bookBack");
         this.measureTool = game.add.bitmapText(0, 0, 'dark', '', FONT_SIZE);
-        this.leftSide = game.add.bitmapText(10, 8, 'dark', '', FONT_SIZE);
-        this.rightSide = game.add.bitmapText(120, 8, 'dark', '', FONT_SIZE);
+        this.leftSide = game.add.bitmapText(PAGE_MARGIN_X, PAGE_MARGIN_Y, 'dark', '', FONT_SIZE);
+        this.rightSide = game.add.bitmapText(RIGHT_PAGE_LOCATION + PAGE_MARGIN_X, PAGE_MARGIN_Y, 'dark', '', FONT_SIZE);
+        
+        this.background.inputEnabled = true;
+        this.background.events.onInputDown.add((element, pointer) => { 
+            const relx = pointer.x - this.layer.x;
+            if (relx > RIGHT_PAGE_LOCATION) {
+                this.nextPages();
+            } else {
+                this.previousPages();
+            }
+        });
         this.leftSide.maxWidth = PAGE_WIDTH;
         this.rightSide.maxWidth = PAGE_WIDTH;
         this.layer.add(this.background);
